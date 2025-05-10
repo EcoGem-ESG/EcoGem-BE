13d15ac984732e59fd2904ef7e58a122ebb97467
minji <20200904@sungshin.ac.kr>
2025-05-11 00:22:45 +0900

    fix:integration with mypage

--- src/main/java/com/ecogem/backend/store/domain/Store.java
+++ src/main/java/com/ecogem/backend/store/domain/Store.java
@@ -3,9 +3,13 @@ package com.ecogem.backend.store.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import com.ecogem.backend.auth.domain.User;
+
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -22,6 +26,9 @@ public class Store {
 
     @Enumerated(EnumType.STRING)
     private DeliveryType deliveryType;
+
+    @OneToMany(mappedBy = "store")
+    private List<User> users;
 
     public enum DeliveryType {
         SMALL, LARGE
--- src/main/java/com/ecogem/backend/store/repository/StoreRepository.java
+++ src/main/java/com/ecogem/backend/store/repository/StoreRepository.java
@@ -2,6 +2,9 @@ package com.ecogem.backend.store.repository;
 
 import com.ecogem.backend.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.ecogem.backend.auth.domain.User;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Store findByUser(User user);
+
 }
